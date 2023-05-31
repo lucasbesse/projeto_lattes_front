1d7bab37befe8da97194f613b09824c255b7ae95
lucasbesse <lucas.bessegato96@gmail.com>
2023-05-30 21:23:08 -0300

    modal de edição

--- projeto_lattes/src/app/pessoa-service/pessoa-service.component.ts
+++ projeto_lattes/src/app/pessoa-service/pessoa-service.component.ts
@@ -14,12 +14,17 @@ export class PessoaServiceComponent {
 
   public showOptions: boolean = false;
 
-  public register: boolean = true;
-  public manage: boolean = false;
+  public register: boolean = false
+  public manage: boolean = true;
+
+  public editModal: boolean = false;
 
   projects = new FormControl('');
   name = new FormControl('');
-  cpf = new FormControl('');
+  email = new FormControl('');
+  start_date = new FormControl('');
+  end_date = new FormControl('');
+  formation = new FormControl('');
 
   projectsList: any = [
     { nome: 'Projeto 1', id: 1 },
@@ -29,23 +34,40 @@ export class PessoaServiceComponent {
   ];
 
   public infos : any = {
-    name: '',
-    cpf: '',
-    projects: ''
+
   }
   
 
   save(){
     this.infos = {
       name: this.name.value,
-      cpf: this.cpf.value,
-      projects: this.projects.value
+      email: this.email.value,
+      projects: this.projects.value,
+      start_date: this.start_date.value,
+      end_date: this.end_date.value,
+      formation: this.formation.value
     }
 
     console.log(this.infos)
   }
 
+
+
   clear(){
 
+  }
+
+  closeEditModal(){
+    this.editModal = false
+  }
+
+  editRegister(e : Event){
+    e.stopPropagation()
+    this.editModal = true
+
+  }
+
+  stopPropagation(e: Event){
+    e.stopPropagation()
   }
 }
